ebf10cb1cc51c7ee3e2a9b1c42f22ca915ca2d18
vishalhassanandani <vishal.hassanandani@gmail.com>
2022-04-12 00:38:12 +0530

    Update ci_platform_portal_build.sh

--- devops/scripts/ci_platform_portal_build.sh
+++ devops/scripts/ci_platform_portal_build.sh
@@ -37,7 +37,7 @@ DEVKITS_DIR=/build/devkits; export DEVKITS_DIR
 #export ANT_HOME=$DEVKITS_DIR/tools/build_software/ant/apache-ant-1.8.4
 #export NODEJS_PATH=$DEVKITS_DIR/tools/build_software/nodejs/v8.11.2/node-v8.11.2-linux-x64/bin
 export BUILD_NUMBER=$BUILD_NUMBER
-export CODE_PATH=${WORKSPACE}/${JOB_NAME}
+export CODE_PATH=${WORKSPACE}
 #export PATH=$PATH:/devkits/tools/build_software/clm_nodejs/nodejs/bin:/devkits/tools/build_software/clm_jruby/jruby/bin
 
 #RELEASEAREA=$cfg_relroot; export RELEASEAREA
