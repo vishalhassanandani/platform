779d2ea2273c5db6cadf08f6819398d496662474
vishalhassanandani <vishal.hassanandani@gmail.com>
2022-04-12 13:31:00 +0530

    Update SimpleClass.java

--- app/SimpleClass.java
+++ app/SimpleClass.java
@@ -1,5 +1,6 @@
 class SimpleClass {
     public static void main(String[] args){
     System.out.println("This is my first program in java");
+        System.out.println("This is my second program in java");
   }
 }
